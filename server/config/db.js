e0d72c4f2cbe78048454eec1acaf859f7c122b0c
SunnyKrS1ngh <usnnyks73@gmail.com>
2024-09-01 23:43:07 +0530

    fixed db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async()=>{
     try{
-        mongoose.set('strictQuery',false);
+        mongoose.set('strictQuery',true);
         const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`Database connected: ${conn.connection.host}`);
 
