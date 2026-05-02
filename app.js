2cfcf50300c05645240ceaea8d39430913056451
SunnyKrS1ngh <usnnyks73@gmail.com>
2026-05-02 23:25:22 +0530

    feat: add dynamic CORS and env example

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-    origin: 'https://mindscribefrontend.onrender.com',  // Allow requests from this origin (frontend)
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true
 }));
 
